5d9530a1ac048fe88e351938439b31b523219328
LM Bibera <lyndon.bibera@headhuntr.io>
2020-08-13 13:16:54 +0800

    Create a struct to hold the story

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,16 +14,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
-    let story0 = "You see a fork in the road."
-    let choice1 = "Take a left."
-    let choice2 = "Take a right."
+    let stories = [
+        Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right.")
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        storyLabel.text = story0
-        choice1Button.setTitle(choice1, for: UIControl.State.normal)
-        choice2Button.setTitle(choice2, for: UIControl.State.normal)
+        storyLabel.text = stories[0].title
+        choice1Button.setTitle(stories[0].choice1, for: UIControl.State.normal)
+        choice2Button.setTitle(stories[0].choice2, for: UIControl.State.normal)
     }
 
 
