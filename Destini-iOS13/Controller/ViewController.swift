54b380d35e8c49bf0fbb16ee797bb5484ca14515
LM Bibera <lyndon.bibera@headhuntr.io>
2020-08-13 13:22:12 +0800

    Add action to button

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -15,17 +15,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice2Button: UIButton!
     
     let stories = [
-        Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right.")
+        Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
+        Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
+        Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")
     ]
+    
+    var currentStory = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        storyLabel.text = stories[0].title
-        choice1Button.setTitle(stories[0].choice1, for: UIControl.State.normal)
-        choice2Button.setTitle(stories[0].choice2, for: UIControl.State.normal)
+        updateUI()
     }
 
-
+    @IBAction func choose(_ sender: UIButton) {
+        currentStory += 1
+        
+        updateUI()
+    }
+    
+    
+    func updateUI() {
+        storyLabel.text = stories[currentStory].title
+        choice1Button.setTitle(stories[currentStory].choice1, for: UIControl.State.normal)
+        choice2Button.setTitle(stories[currentStory].choice2, for: UIControl.State.normal)
+    }
 }
 
